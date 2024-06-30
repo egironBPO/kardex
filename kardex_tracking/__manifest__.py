836a49c134eb30e5f2ee3edbc2838d3f10a75b0e
egironBPO <148819552+egironBPO@users.noreply.github.com>
2024-06-30 14:33:26 -0600

    Add files via upload

---
+++ kardex_tracking/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Kardex de Productos',
+    'version': '15.0',
+    'summary': 'Wizard para visualizar movimientos de Kardex',
+    'description': 'Este módulo permite visualizar los movimientos de Kardex de un producto en un rango de fechas.',
+    'category': 'Inventory',
+    'author': 'Elder Armando Giron',
+    'price': '48.99',
+    'currency': 'USD',
+    'website': '',
+    'depends': ['stock', 'point_of_sale'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_kardex_wizard_views.xml',
+        'views/menu_views.xml',
+        'views/kardex_tracking_views.xml'
+    ],
+    'images':['static/description/banner.gif'],
+    'installable': True,
+    'application': False,
+    
+}
